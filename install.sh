e4eef35c7474fa567299ed775f48bc4a246307f2
Vitor Nere <vitor.nere@hotmail.com>
2017-06-02 18:09:26 -0300

    Melhorado o install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "Instalando..."
+chmod +x *
 mkdir -p $HOME/ejudge $HOME/bin
 rm -rf $HOME/ejudge/bin
 cp -rf ejudge_modules/ $HOME/ejudge/bin
 cp ejudge ejudge.py $HOME/bin
+echo "Adicionando ejudge ao PATH..."
+export PATH=$PATH:$HOME/bin
+echo $PATH
+echo "Feche e abra o terminal para finalizar a instalação..."

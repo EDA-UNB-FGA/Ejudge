5f8f54ffaafecf5fd0a7999c1056aebcdc21d7a0
Vitor Nere <vitor.nere@hotmail.com>
2017-06-27 11:57:36 -0300

    Modificado forma de inserção no .bashrc

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ rm -rf $HOME/ejudge/bin
 cp -rf ejudge_modules/ $HOME/ejudge/bin
 cp ejudge ejudge.py $HOME/bin
 echo "Adicionando ejudge ao PATH..."
-export PATH=$PATH:$HOME/bin
+printf '\nif [ -d "$HOME/bin" ] ; then' >> ~/.bashrc
+printf '\nPATH=$HOME/bin:$PATH' >> ~/.bashrc
+printf '\nfi' >> ~/.bashrc
 echo $PATH
 echo "Feche e abra o terminal para finalizar a instalação..."

bd0dc87f83615ba6256853902d9625d1040ec10b
bumbleblo <bumbleblo2013@gmail.com>
2017-09-20 21:06:50 -0300

    Novo arquivo do Ejudge para teste

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "Instalando..."
-chmod +x *
 mkdir -p $HOME/ejudge $HOME/bin
 rm -rf $HOME/ejudge/bin
 cp -rf ejudge_modules/ $HOME/ejudge/bin
 cp ejudge ejudge.py $HOME/bin
-echo "Adicionando ejudge ao PATH..."
-printf '\nif [ -d "$HOME/bin" ] ; then' >> ~/.bashrc
-printf '\nPATH=$HOME/bin:$PATH' >> ~/.bashrc
-printf '\nfi\n' >> ~/.bashrc
-source ~/.bashrc
-echo "Instalação completa."
